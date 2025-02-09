07b368cd0b2d511e3c3ea8452b7e362861989d4d
Chandan gupta <cgupta4556@gmail.com>
2025-02-10 04:25:51 +0530

    Updates

---
+++ Grocery.4d7d1e88.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2;(0,e.register)("49riL",function(i,n){Object.defineProperty(i.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(i.exports,"default",{get:function(){return r},set:void 0,enumerable:!0,configurable:!0});var o=e("5aRSq"),r=()=>(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"Our Grocery page with multiple child components."})})});
+//# sourceMappingURL=Grocery.4d7d1e88.js.map
